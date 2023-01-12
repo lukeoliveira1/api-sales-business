975269e581218a30f987fd00f4e235ce89ca9b83
Lucas <lucas@solarz.com.br>
2023-01-12 17:23:01 -0300

    entities and migration

---
+++ src/main/java/com/business/sales/api/salesBusinessApi/SalesBusinessApiApplication.java
@@ -0,0 +1,13 @@
+package com.business.sales.api.salesBusinessApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesBusinessApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesBusinessApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/business/sales/api/salesBusinessApi/SalesBusinessApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.business.sales.api.salesBusinessApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesBusinessApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
