047e99ea4afb12e3aec09b70322c95264a0107b0
Lucas <lukeoliveira1>
2023-02-15 09:12:27 -0300

    add configuração de cors

--- src/main/java/com/salesBusinessApi/salesBusinessApiApplication.java
+++ src/main/java/com/salesBusinessApi/salesBusinessApiApplication.java
@@ -2,6 +2,9 @@ package com.salesBusinessApi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 
@@ -11,4 +14,13 @@ public class salesBusinessApiApplication {
 		SpringApplication.run(salesBusinessApiApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST","PUT","DELETE","PATCH");
+			}
+		};
+	}
 }
